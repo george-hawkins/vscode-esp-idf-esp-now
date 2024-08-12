2179409b22176f227cf5d6a8395eb3c82c114b1d
George Hawkins <george-hawkins@users.noreply.github.com>
2024-08-12 14:14:51 +0200

    Removed references to coin-cell board from switch.c.

--- examples/coin_cell_clean/switch/main/switch.c
+++ examples/coin_cell_clean/switch/main/switch.c
@@ -9,9 +9,7 @@
 #include "esp_wifi.h"
 #include "esp_sleep.h"
 
-#if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(4, 4, 0)
 #include "esp_mac.h"
-#endif
 
 #include "espnow_utils.h"
 
@@ -19,8 +17,6 @@
 #include "espnow_ctrl.h"
 
 #include "iot_button.h"
-
-#include "switch_board.h"
 
 #define BULB_STATUS_KEY       "bulb_key"
 
@@ -39,113 +35,6 @@ static void app_wifi_init()
     ESP_ERROR_CHECK(esp_wifi_start());
 }
 
-#ifdef CONFIG_EXAMPLE_USE_COIN_CELL_BUTTON
-#define SEND_GAP_TIME         30
-#define LONG_PRESS_SLEEP_TIME 2000
-
-typedef enum {
-    ESPNOW_TASK_STATE_SEND_RECORD,
-    ESPNOW_TASK_STATE_SEND_SCAN,
-    ESPNOW_TASK_STATE_SEND_BIND,
-    ESPNOW_TASK_STATE_BIND_DONE,
-    ESPNOW_TASK_STATE_SEND_UNBIND,
-    ESPNOW_TASK_STATE_UNBIND_DONE,
-    ESPNOW_TASK_STATE_DONE,
-} espnow_task_state_t;
-
-static espnow_task_state_t task_state = ESPNOW_TASK_STATE_SEND_RECORD;
-
-static void set_light_sleep(uint32_t ms)
-{
-    esp_wifi_force_wakeup_release();
-    esp_sleep_enable_timer_wakeup(ms * 1000);
-    esp_light_sleep_start();
-    esp_wifi_force_wakeup_acquire();
-    esp_sleep_wakeup_cause_t cause = esp_sleep_get_wakeup_cause();
-    const char *cause_str;
-    switch (cause) {
-    case ESP_SLEEP_WAKEUP_GPIO:
-        cause_str = "GPIO";
-        break;
-    case ESP_SLEEP_WAKEUP_UART:
-        cause_str = "UART";
-        break;
-    case ESP_SLEEP_WAKEUP_TIMER:
-        cause_str = "timer";
-        break;
-    default:
-        cause_str = "unknown";
-        break;
-    }
-    ESP_LOGI(TAG, "Woke up from: %s", cause_str);
-}
-
-static void control_task(void *pvParameter)
-{
-    board_led_on(true);
-    board_power_lock(true);
-    set_light_sleep(SEND_GAP_TIME);
-
-    for (;;) {
-        if (task_state == ESPNOW_TASK_STATE_SEND_RECORD) {
-            uint8_t status = 0;
-            /* status = 0: OFF, 1: ON, 2: TOGGLE */
-#if CONFIG_EXAMPLE_SWITCH_STATUS_PERSISTED
-            espnow_storage_get(BULB_STATUS_KEY, &status, sizeof(status));
-            status ^= 1;
-            espnow_storage_set(BULB_STATUS_KEY, &status, sizeof(status));
-#else
-            status = 2;
-#endif
-            ESP_LOGI(TAG, "switch send press");
-            espnow_ctrl_initiator_send(ESPNOW_ATTRIBUTE_KEY_1, ESPNOW_ATTRIBUTE_POWER, status);
-
-            task_state = ESPNOW_TASK_STATE_DONE;
-        }
-
-        if (task_state == ESPNOW_TASK_STATE_DONE) {
-            set_light_sleep(SEND_GAP_TIME); //Prevent false wake-ups
-            board_power_lock(false);
-            board_led_on(false);
-            ESP_LOGW(TAG, "task_state = ESPNOW_TASK_STATE_DONE");
-            set_light_sleep(LONG_PRESS_SLEEP_TIME);
-            board_power_lock(true);
-            board_led_on(true);
-            task_state = ESPNOW_TASK_STATE_SEND_BIND;
-        }
-
-        if (task_state == ESPNOW_TASK_STATE_SEND_BIND) {
-            ESP_LOGI(TAG, "switch bind press");
-            espnow_ctrl_initiator_bind(ESPNOW_ATTRIBUTE_KEY_1, true);
-            task_state = ESPNOW_TASK_STATE_BIND_DONE;
-        }
-
-        if (task_state == ESPNOW_TASK_STATE_BIND_DONE) {
-            set_light_sleep(SEND_GAP_TIME); //Prevent false wake-ups
-            board_power_lock(false);
-            board_led_on(false);
-            ESP_LOGW(TAG, "task_state = ESPNOW_TASK_STATE_DONE");
-            set_light_sleep(LONG_PRESS_SLEEP_TIME);
-            board_power_lock(true);
-            board_led_on(true);
-            task_state = ESPNOW_TASK_STATE_SEND_UNBIND;
-        }
-
-        if (task_state == ESPNOW_TASK_STATE_SEND_UNBIND) {
-            ESP_LOGI(TAG, "switch unbind press");
-            espnow_ctrl_initiator_bind(ESPNOW_ATTRIBUTE_KEY_1, false);
-            task_state = ESPNOW_TASK_STATE_UNBIND_DONE;
-        }
-
-        if (task_state == ESPNOW_TASK_STATE_UNBIND_DONE) {
-            set_light_sleep(SEND_GAP_TIME); //Prevent false wake-ups
-            board_power_lock(false);
-            board_led_on(false);
-            set_light_sleep(LONG_PRESS_SLEEP_TIME);
-        }
-    }
-}
-#else
 // All the default GPIOs are based on ESP32 series DevKitC boards, for other boards, please modify them accordingly.
 #ifdef CONFIG_IDF_TARGET_ESP32C2
 #define CONTROL_KEY_GPIO      GPIO_NUM_9
@@ -169,13 +58,9 @@ static void app_switch_send_press_cb(void *arg, void *usr_data)
 
     ESP_LOGI(TAG, "switch send press");
     /* status = 0: OFF, 1: ON, 2: TOGGLE */
-#if CONFIG_EXAMPLE_SWITCH_STATUS_PERSISTED
     espnow_storage_get(BULB_STATUS_KEY, &status, sizeof(status));
     status ^= 1;
     espnow_storage_set(BULB_STATUS_KEY, &status, sizeof(status));
-#else
-    status = 2;
-#endif
     ESP_LOGI(TAG, "key status: %d", status);
     espnow_ctrl_initiator_send(ESPNOW_ATTRIBUTE_KEY_1, ESPNOW_ATTRIBUTE_POWER, status);
 }
@@ -195,13 +80,9 @@ static void app_switch_unbind_press_cb(void *arg, void *usr_data)
     ESP_LOGI(TAG, "switch unbind press");
     espnow_ctrl_initiator_bind(ESPNOW_ATTRIBUTE_KEY_1, false);
 }
-#endif
 
 static void app_driver_init(void)
 {
-#ifdef CONFIG_EXAMPLE_USE_COIN_CELL_BUTTON
-    board_init();
-#else
     button_config_t button_config = {
         .type = BUTTON_TYPE_GPIO,
         .gpio_button_config = {
@@ -215,7 +96,6 @@ static void app_driver_init(void)
     iot_button_register_cb(button_handle, BUTTON_SINGLE_CLICK, app_switch_send_press_cb, NULL);
     iot_button_register_cb(button_handle, BUTTON_DOUBLE_CLICK, app_switch_bind_press_cb, NULL);
     iot_button_register_cb(button_handle, BUTTON_LONG_PRESS_START, app_switch_unbind_press_cb, NULL);
-#endif
 }
 
 void app_main(void)
@@ -229,8 +109,4 @@ void app_main(void)
     espnow_config.send_max_timeout = portMAX_DELAY;
     espnow_init(&espnow_config);
     esp_now_set_wake_window(0);
-
-#ifdef CONFIG_EXAMPLE_USE_COIN_CELL_BUTTON
-    xTaskCreate(control_task, "control_task", 4096*2, NULL, 15, NULL);
-#endif
 }
